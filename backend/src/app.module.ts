7b10233c2dd09a7dae25b42a03c2e2f03be22b2a
_nocillax_mba <asifjarif@gmail.com>
2025-10-27 14:18:46 +0600

    initial backend setup

---
+++ backend/src/app.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './database/entities/user.entity';
+import { Inventory } from './database/entities/inventory.entity';
+import { Item } from './database/entities/item.entity';
+import { Comment } from './database/entities/comment.entity';
+import { Access } from './database/entities/access.entity';
+import { CategoryLookup } from './database/entities/category_lookup.entity';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { InventoryModule } from './modules/inventory/inventory.module';
+import { ItemModule } from './modules/item/item.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        entities: [User, Inventory, Item, Comment, Access, CategoryLookup],
+        synchronize: true,
+        logging: false,
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    AuthModule,
+    InventoryModule,
+    ItemModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ backend/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN!.split(','),
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT!);
+}
+bootstrap();
